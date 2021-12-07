0e858692108f7fbe9220ba5e41b649d3f2ee2743
cbbcbail <cbbcbail@gmail.com>
2021-12-07 16:09:28 -0600

    Update labellingFunctions.py

--- labellingFunctions.py
+++ labellingFunctions.py
@@ -5,36 +5,36 @@ from scipy.signal import find_peaks
 from skimage import filters
 
 # --- Noisy Labelling Functions for MNIST dataset ------------------------------
-def fillCount(x): # fills enclosed parts and counts pixels > 0
-    x=x.reshape(28,28)
-    seed =x.copy()
-    seed[1:-1, 1:-1] = x.max()
-    mask = x
-
-    filled = reconstruction(seed, mask, method='erosion')
-    count = 0
-    for i in filled:
-        for j in i:
-          if j > 0:
-            count += 1
-    return count
-
-def fillSum(x): # fills enclosed parts and sums grayscale image
-    x = x.reshape(28, 28)
-    seed =x.copy()
-    seed[1:-1, 1:-1] = x.max()
-    mask = x
-    filled = reconstruction(seed, mask, method='erosion')
-    return np.sum(filled)
-
 def pixelCount(x): # counts pixels > 0 in the image
-    image=x.reshape(28,28)
+    image = x.reshape(28,28)
     count = 0
     for i in image:
         for j in i:
             if j > 0:
                 count += 1
     return count
+
+def fill(image):
+    seed = x.copy()
+    seed[1:-1, 1:-1] = x.max()
+    mask = x
+    filledImage = reconstruction(seed, mask, method='erosion')
+    return filledImage
+
+def fillCount(x): # fills enclosed parts and counts pixels > 0
+    image = x.reshape(28,28)
+    filledImage = fill(image)
+    count = 0
+    for i in filledImage:
+        for j in i:
+          if j > 0:
+            count += 1
+    return count
+
+def fillSum(x): # fills enclosed parts and sums grayscale image
+    image = x.reshape(28, 28)
+    filled = fill(image)
+    return np.sum(filled)
 
 # calculates euclidean distance of each image from the _0_ matrix
 # this is the L2 norm
@@ -48,11 +48,8 @@ def euclideanBinarize(x):
     zeroImage = x.copy() * 0
     return np.sum((zeroImage - x) ** 2)
 
-
-
-# counts peaks in the horizontal direction of the image
-def horizontalPeakCount(x):
-    x=x.reshape(28,28)
+# counts peaks in count of pixels > 0 in one direction of the image
+def peakCount(x):
     counts = []
     for i in x:
         count = 0
@@ -60,28 +57,26 @@ def horizontalPeakCount(x):
             if j > 0:
                 count += 1
         counts.append(count)
-    return len(find_peaks(counts)[0])
+    numPeaks = len(find_peaks(counts)[0])
+    return numPeaks
+
+# counts peaks in the horizontal direction of the image
+def horizontalPeakCount(x):
+    image = x.reshape(28,28)
+    return peakCount(image)
 
 # counts peaks in the vertical direction of the image
 def verticalPeakCount(x):
-    image=x.reshape(28,28).T
-    counts = []
-    # image = np.copy(image).transpose()
-    for i in image:
-        count = 0
-        for j in i:
-            if j > 0:
-                count += 1
-        counts.append(count)
-    return len(find_peaks(counts)[0])
+    image = x.reshape(28,28).T
+    return peakCount(image)
 
 # returns ratio of peaks in horizontal direction to peaks in vertical direction
-def ratioPeakCount(image):
-  vpc = verticalPeakCount(image)
-  if vpc == 0:
-    return horizontalPeakCount(image)
-  else:
-    return horizontalPeakCount(image)/vpc
+def ratioPeakCount(x):
+    vpc = verticalPeakCount(x)
+    if vpc == 0:
+        return horizontalPeakCount(x)
+    else:
+        return horizontalPeakCount(x)/vpc
 
 # does roberts edge detection and counts peaks in the vertical direction
 def edgeDetectVertical(x):
